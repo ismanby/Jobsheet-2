96096246b2d8e2c503679ebbfbfc68485345f5ff
ismanby <ismaanby@gmail.com>
2026-02-20 08:56:49 +0700

    Update Percobaan 3

--- Mahasiswa12.java
+++ Mahasiswa12.java
@@ -4,7 +4,16 @@ public class Mahasiswa12 {
     String kelas;
     double ipk;
 
+    public Mahasiswa12() {
 
+    }
+    public Mahasiswa12(String nm, String nim, double ipk, String kls) {
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
+    
     void tampilkanInformasi() {
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
